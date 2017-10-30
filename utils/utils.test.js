abaa5e09d6552b3c9e42619dab11b8b20d60e79d
Ankit Arora <ankit992@live.in>
2017-10-31 02:08:14 +0530

    adding describe blocks and using describe to format tests

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,51 +1,58 @@
 const expect = require('expect');
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-    var res = utils.add(3,4);
-    expect(res).toBe(7).toBeA('number');
-});
+describe('Utils', () => {
 
-it('should square a number', () => {
-    var res = utils.square(5);
-    expect(res).toBe(25).toBeA('number');
-});
-
-it('should async add two numbers', (done) => {
-    utils.asyncAdd(4, 5, (sum) => {
-        expect(sum).toBe(9).toBeA('number');
-        done();
+    describe('#add', () => {
+        it('should add two numbers', () => {
+            var res = utils.add(3,4);
+            expect(res).toBe(7).toBeA('number');
+        });    
     });
-});
 
-it('should async square a numbet', (done) => {
-    utils.asyncSquare(5, (square) => {
-        expect(square).toBe(25).toBeA('number');
-        done();
+    it('should square a number', () => {
+        var res = utils.square(5);
+        expect(res).toBe(25).toBeA('number');
     });
-});
-
-it('should verify first and last names are set', () => {
-    var user = {location: 'India', age: 25};
-    var res = utils.setName(user, 'Ankit Arora' );
-    expect(user).toEqual(res);
-    expect(res).toInclude({
-        firstName: 'Ankit',
-        lastName: 'Arora',
-        location: 'India',
-        age: 25
+    
+    it('should async add two numbers', (done) => {
+        utils.asyncAdd(4, 5, (sum) => {
+            expect(sum).toBe(9).toBeA('number');
+            done();
+        });
     });
+    
+    it('should async square a number', (done) => {
+        utils.asyncSquare(5, (square) => {
+            expect(square).toBe(25).toBeA('number');
+            done();
+        });
+    });
+    
+    it('should verify first and last names are set', () => {
+        var user = {location: 'India', age: 25};
+        var res = utils.setName(user, 'Ankit Arora' );
+        expect(user).toEqual(res);
+        expect(res).toInclude({
+            firstName: 'Ankit',
+            lastName: 'Arora',
+            location: 'India',
+            age: 25
+        });
+    });
+    
+    // it('should expect some values', () => {
+    //     // expect(12).toNotBe(11);
+    //     // expect({name: 'Ankit'}).toNotEqual({name: 'ankit'});
+    //     // expect([2,3,4]).toExclude(1);
+    //     expect({
+    //         name: 'Ankit',
+    //         age: 25, 
+    //         location: 'Gurugram'
+    //     }).toExclude({
+    //         age: 22
+    //     });
+    // });
+
 });
 
-// it('should expect some values', () => {
-//     // expect(12).toNotBe(11);
-//     // expect({name: 'Ankit'}).toNotEqual({name: 'ankit'});
-//     // expect([2,3,4]).toExclude(1);
-//     expect({
-//         name: 'Ankit',
-//         age: 25, 
-//         location: 'Gurugram'
-//     }).toExclude({
-//         age: 22
-//     });
-// });
